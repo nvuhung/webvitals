17e8817dd7c154843d954fce86e59d8d96ef4475
nvuhung <nvuhung@outlook.com>
2021-07-16 15:49:53 +0700

    fix: initialize gtm 1 time

--- src/index.js
+++ src/index.js
@@ -13,19 +13,19 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+TagManager.initialize({ gtmId: 'GTM-KXCXCHN' });
+
 function sendToAnalytics({ id, name, value }) {
-  const tagManagerArgs = {
-    gtmId: 'GTM-KXCXCHN',
-    dataLayer: {
-      eventCategory: 'Web Vitals',
-      eventAction: name,
-      eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-      eventLabel: id, // id unique to current page load
-      nonInteraction: true, // avoids affecting bounce rate
-    }
-  };
- 
-  TagManager.initialize(tagManagerArgs)
+  window.dataLayer = window.dataLayer || [];
+
+  window.dataLayer.push({
+    event: 'web-vitals',
+    eventCategory: 'Web Vitals',
+    eventAction: name,
+    eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    eventLabel: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate
+  });
 }
 
 reportWebVitals(sendToAnalytics);
