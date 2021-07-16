56cd8366c93e1c6e2720ffca403c819556884ec0
nvuhung <nvuhung@outlook.com>
2021-07-16 15:18:39 +0700

    chore: first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TagManager from 'react-gtm-module'
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,7 +13,19 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function sendToAnalytics({ id, name, value }) {
+  const tagManagerArgs = {
+    gtmId: 'GTM-KXCXCHN',
+    dataLayer: {
+      eventCategory: 'Web Vitals',
+      eventAction: name,
+      eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+      eventLabel: id, // id unique to current page load
+      nonInteraction: true, // avoids affecting bounce rate
+    }
+  };
+ 
+  TagManager.initialize(tagManagerArgs)
+}
+
+reportWebVitals(sendToAnalytics);
