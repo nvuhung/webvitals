b8b63de60db8a0f772d039bee9af655ff06020c7
nvuhung <nvuhung@outlook.com>
2021-07-17 10:47:14 +0700

    chore: update web vitals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TagManager from 'react-gtm-module'
+import TagManager from 'react-gtm-module';
 
 import './index.css';
 import App from './App';
@@ -10,24 +10,28 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 TagManager.initialize({ gtmId: 'GTM-KXCXCHN' });
 
 function sendToAnalytics(event) {
-  console.log('event :>> ', event);
-  const { id, name, value } = event;
+  const { id, name, value, delta } = event;
 
   window.dataLayer = window.dataLayer || [];
+  const webVitalsMeasurement = {
+    name,
+    id,
+    value,
+    delta,
+    valueRounded: Math.round(name === 'CLS' ? value * 1000 : value),
+    deltaRounded: Math.round(name === 'CLS' ? value * 1000 : value),
+  };
+  console.log('webVitalsMeasurement :>> ', webVitalsMeasurement);
 
   window.dataLayer.push({
     event: 'web-vitals',
-    eventCategory: 'Web Vitals',
-    eventAction: name,
-    eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
-    eventLabel: id, // id unique to current page load
-    nonInteraction: true, // avoids affecting bounce rate
+    webVitalsMeasurement,
   });
 }
 
